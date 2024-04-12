83837bb24ee998622e688ab9c6f5d16618cab3b7
xuguojing-xgj <xuguojing77@163.com>
2024-04-12 23:16:10 +0800

    init project

---
+++ src/app.module.ts
@@ -0,0 +1,11 @@
+// T 应用程序的根模块 (root module)
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
